0c80fee19a122168fbe343e5df6370f4eca8254e
moose735 <132318332+moose735@users.noreply.github.com>
2025-06-12 07:38:41 -0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="mt-8 text-center text-gray-600 text-sm pb-8">
+      {/* Your footer content, if any, can go here */}
+    </footer>
+  );
+};
+
+export default Footer;
