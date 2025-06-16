7596e423d34c8cd6db337b6e373c664c6cd467e1
moose735 <132318332+moose735@users.noreply.github.com>
2025-06-16 14:18:13 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,10 +7,6 @@ export const GOOGLE_SHEET_POWER_RANKINGS_API_URL = 'https://script.google.com/ma
 // Google Apps Script URL for All Historical Matchups (used by MatchupHistory, RecordBook, DPRAnalysis, LuckRatingAnalysis, TeamDetailPage)
 export const HISTORICAL_MATCHUPS_API_URL = 'https://script.google.com/macros/s/AKfycbxpo21zzZgNamYShESfqe-SX09miJz2LK7SpdlYrtHXQplneB3bF2xu2byy0HhjM8e-/exec';
 
-// If you have a Google Sheet for Champions/Awards, add its Apps Script URL here.
-// Otherwise, mock data will be used in TeamDetailPage.
-export const GOOGLE_SHEET_CHAMPIONS_API_URL = 'YOUR_GOOGLE_SHEET_CHAMPIONS_API_URL_OPTIONAL';
-
 // Custom team name mappings (Optional: for use if you want to map names from historical matchups to custom display names)
 // Example: { "TeamA_from_Google_Sheet": "My Awesome Team", "TeamB_from_Google_Sheet": "Gridiron Heroes" }
 // App.js will currently use names directly from HISTORICAL_MATCHUPS_API_URL for dropdown and display.
