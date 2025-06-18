2b922752abb0a9be6e748e45c7ac5879e145bf37
moose735 <132318332+moose735@users.noreply.github.com>
2025-06-18 11:32:10 -0400

    Update LeagueHistory.js

--- src/lib/LeagueHistory.js
+++ src/lib/LeagueHistory.js
@@ -544,7 +544,7 @@ const LeagueHistory = ({ historicalMatchups, loading, error, getDisplayTeamName 
               <ResponsiveContainer width="100%" aspect={4 / 3}>
                 <LineChart
                   data={seasonalDPRChartData}
-                  margin={{ top: 18, right: 10, left: 10, bottom: 10 }}
+                  margin={{ top: 20, right: 10, left: 10, bottom: 5 }}
                 >
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="year" label={{ value: "Season", position: "insideBottom", offset: 0 }} />
