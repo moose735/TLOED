cfa3c0b440be4baaed68b7fa26048be8e10a47fa
moose735 <132318332+moose735@users.noreply.github.com>
2025-07-07 13:07:50 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,8 @@ export const GOOGLE_SHEET_POWER_RANKINGS_API_URL = 'https://script.google.com/ma
 // Google Apps Script URL for All Historical Matchups (used by MatchupHistory, RecordBook, DPRAnalysis, LuckRatingAnalysis, TeamDetailPage)
 export const HISTORICAL_MATCHUPS_API_URL = 'https://script.google.com/macros/s/AKfycbxpo21zzZgNamYShESfqe-SX09miJz2LK7SpdlYrtHXQplneB3bF2xu2byy0HhjM8e-/exec';
 
+export const LEAGUE_START_YEAR = 2021;
+
 // Custom team name mappings (Optional: for use if you want to map names from historical matchups to custom display names)
 // Example: { "TeamA_from_Google_Sheet": "My Awesome Team", "TeamB_from_Google_Sheet": "Gridiron Heroes" }
 // App.js will currently use names directly from HISTORICAL_MATCHUPS_API_URL for dropdown and display.
